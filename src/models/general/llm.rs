1bd08e70b947455ac041618d7d74004602985d60
aryan-212 <allalonearyan@gmail.com>
2025-03-06 00:07:03 +0530

    request layout setup

--- src/models/general/llm.rs
+++ src/models/general/llm.rs
@@ -1,3 +1,28 @@
-use serde::{Deserialize, Serialize};
-#[derive(Debug, Serialize, Clone)]
-pub struct Message {}
+use reqwest::{Client, header};
+use std::env;
+use std::error::Error;
+
+pub async fn send_request(prompt: &str) -> Result<String, Box<dyn Error>> {
+    let api_key = env::var("GEMINI_API_KEY")?;
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, "application/json".parse()?);
+
+    let client = Client::new();
+    let url = format!(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={}",
+        api_key
+    );
+    let body = format!(r#"{{"contents":[{{"parts":[{{"text":"{}"}}]}}]}}"#, prompt);
+
+    let response = client
+        .post(&url)
+        .headers(headers)
+        .body(body)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(response)
+}
